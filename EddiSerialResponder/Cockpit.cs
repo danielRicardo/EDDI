7320a37635baf35c5902ee6b02c99d72b327739b
Daniel Ricardo <daniel.k.ricardo@gmail.com>
2020-01-06 23:44:43 +0200

    Create the Cockpit class

---
+++ EddiSerialResponder/Cockpit.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace EddiSerialResponder
+{
+    public class Cockpit
+    {
+        [JsonProperty("name")]
+        public string Name { get; private set; }
+
+        [JsonProperty("description")]
+        public string Description { get; private set; }
+        
+
+    }
+}
